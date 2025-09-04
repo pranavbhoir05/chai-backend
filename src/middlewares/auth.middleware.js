72f16025b59908c8211d5e4bc76a75fb8680d162
Pranav Bhoir <pranavbhoir182000@gmail.com>
2025-09-05 00:35:25 +0530

    resolved merge conflicts

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
@@ -46,52 +45,3 @@ export const verifyJWT = asyncHandler(async (req, _ ,
 // req.cookies  	✅ Yes   	Object containing all cookies
 // req.cookie	    ❌ No	    Not defined — will return undefined
 
-=======
-import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken";
-import { User } from "../models/user.model.js";
-
- 
-// why or how someone can export a function directly without assigning it to a const (or function) first.
-// 1. JavaScript allows you to export a value directly, and in this case, you're exporting an anonymous arrow function.
-// 2. It’s equivalent to assigning it to a variable first, but cleaner for simple cases.
-
-
-
-export const verifyJWT = asyncHandler(async (req, _ , 
-    next) => { 
-     
-   try {
-     const token = req.cookies?.accessToken || req.header
-     ("Authorization")?.replace("Bearer ", "")
-     if(!token){
-         throw new ApiError(401," unauthorized request")
-     }
-     const decodedToken = jwt.verify(token, process.env.
-         ACCESS_TOKEN_SECRET)
-    const user =  await User.findById(decodedToken?._id).
-    select("-password -refreshToken")
-    if(!user){
-     //next_video : discuss about frontend
-     throw new ApiError(401," invalid access token")
- }    
-     req.user = user
-     next()
-   } catch (error) {
-        throw new ApiError(401, error?.message || "invalid access token")
-   }
-    //im giving access of user to req object(user)
-})
-
-
-
-
-//❌ req.cookie (singular) does not exist
-// If you try it, you'll get undefined
-
-// Property  	     Exists?     	Description
-// req.cookies  	✅ Yes   	Object containing all cookies
-// req.cookie	    ❌ No	    Not defined — will return undefined
-
->>>>>>> e861606db0969017339e2c49d41a2d64eea65552
